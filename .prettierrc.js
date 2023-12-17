3715db43d10cee34d9014cbd5d3c41828ef71d7a
Lauren Hamel <lauren.hamel@outlook.com>
2023-12-17 10:41:25 -0500

    Adds editorconfig and prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  options: {
+    editorconfig: true,
+  },
+};
